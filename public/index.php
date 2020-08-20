08e89759067698dc1cefb3098b22f240caa657b5
Giuseppe <giuseppe@techtribe.nl>
2020-08-20 17:00:10 +0200

    enabled Body Parsing Middleware

--- public/index.php
+++ public/index.php
@@ -60,6 +60,9 @@ $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory);
 $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
 register_shutdown_function($shutdownHandler);
 
+// Add Body Parsing Middleware
+$app->addBodyParsingMiddleware();
+
 // Add Routing Middleware
 $app->addRoutingMiddleware();
 
